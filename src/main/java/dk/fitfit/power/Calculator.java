9b4aa8e5e24a9314e97f3e28b4e5af1a1a4724e6
Sebastian Jensen <sebastianthegreatful@gmail.com>
2017-07-12 18:30:16 +0200

    Initial commit.

---
+++ src/main/java/dk/fitfit/power/Calculator.java
@@ -0,0 +1,8 @@
+package dk.fitfit.power;
+
+public class Calculator {
+	public int add(int a, int b) {
+		return MathUtil.addInteger(a, b);
+	}
+}
+
---
+++ src/main/java/dk/fitfit/power/MathUtil.java
@@ -0,0 +1,10 @@
+package dk.fitfit.power;
+
+public abstract class MathUtil {
+	public static final int addInteger(int a, int b) {
+		return a + b;
+	}
+
+	private MathUtil() {
+	}
+}
